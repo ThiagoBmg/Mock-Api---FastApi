b057d776a8cd45670179c5d6c0c1884c1afdb0c3
ThiagoBmg <th.bmg5274@gmail.com>
2022-03-08 12:43:21 -0300

    adicionando decouple

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 def get_api_key(api_key):
     if api_key != SECRET_KEY:
